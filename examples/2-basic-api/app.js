f2261a2a94ea164454b88a49881ae484ffd7774d
Michiko Taguchi <michytaguchi@gmail.com>
2021-11-22 21:32:38 -0800

    first commit

---
+++ examples/2-basic-api/app.js
@@ -0,0 +1,22 @@
+const result = document.querySelector('.result')
+
+const fetchData = async () => {
+  try{
+    const {data} = await axios.get('/api/2-basic-api');
+    const products = data.map((product)=>{
+      const {image:{url},name,price}= product
+      return `<article class="product">
+      <img src="${url}" alt="${name}"/>
+      <div class="info">
+      <h5>${name}</h5>
+      <h5 class="price">$${price}</h5>
+      </div></article>`
+    }).join('')
+    console.log(data)
+    result.innerHTML = products
+  }catch(error){
+    result.innerHTML = `<h2>There was an error. Please try again.</2>`
+  }
+}
+
+fetchData()
